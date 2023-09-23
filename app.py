f63fe1050699cda5a0482c9e8f125c0d345cc7d6
Mubashir Tanwar <tanwar0210@gmail.com>
2023-09-23 21:29:16 +0530

    Update Prompt Template

--- app.py
+++ app.py
@@ -32,15 +32,12 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
@@ -52,7 +49,8 @@ def get_conversation_chain(vectorstore):
 
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
+    promtTemplate = "" + user_question
+    response = st.session_state.conversation({'question': promtTemplate})
     st.session_state.chat_history = response['chat_history']
 
     for i, message in enumerate(st.session_state.chat_history):
@@ -67,8 +65,7 @@ def handle_userinput(user_question):
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chanakya Chat",
-                       page_icon=":robot_face:",
-                       initial_sidebar_state="collapsed")
+                       page_icon=":robot_face:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -77,26 +74,21 @@ def main():
         st.session_state.chat_history = None
 
     st.header("Chanakya")
+    pdf_docs = ["docs/dbms-book.pdf", "docs/syllubusAIML.pdf"]
+    if st.button("Start"):
+        with st.spinner("Analyzing"):
+            raw_text = get_pdf_text(pdf_docs)
+            text_chunks = get_text_chunks(raw_text)
+            vectorstore = get_vectorstore(text_chunks)
+            st.session_state.conversation = get_conversation_chain(
+                vectorstore)
+        st.empty()
     user_question = st.text_input("Chat with Chanakya:")
     if user_question:
         handle_userinput(user_question)
 
-    with st.sidebar:
-        pdf_docs = ["docs/Shashank_Gupta_Resume.pdf"]
-        if st.button("Start"):
-            with st.spinner("Analyzing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
-
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
 
 
 if __name__ == '__main__':
